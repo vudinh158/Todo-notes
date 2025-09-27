90baf719ec49e8114e8eef3e54e9f4e11047ff73
vudinh158 <vudinhtran2312@gmail.com>
2025-09-27 21:19:44 +0700

    fix server latest

--- server/src/server.js
+++ server/src/server.js
@@ -7,8 +7,29 @@ import todoRouter from "./routes/todo.routes.js";
 
 dotenv.config();
 
+const allowedOrigins = [
+  "https://todo-notes-khaki.vercel.app/", // domain chính
+  "https://todo-notes-git-main-vudinhtran2312-gmailcoms-projects.vercel.app/", // domain preview
+  "http://localhost:5173" // để test local FE
+];
+
 const app = express();
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (
+        !origin ||
+        origin.endsWith(".vercel.app") ||
+        allowedOrigins.includes(origin)
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
